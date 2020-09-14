df8b4066871b224cbddf18fc316b74d9bcd55fcb
git <robidev@github.com>
2020-09-14 22:53:05 +0200

    added enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 800f;
+    public float nextWaypointDistance = 3f;
+
+    public Transform enemySprite;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndofPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if(seeker.IsDone())
+        {
+            seeker.StartPath(rb.position,target.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(path == null)
+            return;
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndofPath = true;
+            return;
+        } 
+        else
+        {
+            reachedEndofPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if(force.x >= 0.01f)
+        {
+            enemySprite.localScale = new Vector3(-1f,1f,1f);
+        }
+        else if (force.x <= -0.01f)
+        {
+            enemySprite.localScale = new Vector3(1f,1f,1f);
+        }
+    }
+}
