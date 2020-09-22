145c00ba8a0ca8a55d432a94036812ab15ad501b
git <robidev@github.com>
2020-09-22 23:16:31 +0200

    added smarter bird ai

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -10,10 +10,13 @@ public class EnemyAI : MonoBehaviour
     public Transform[] PatrolWaypoints;
     public float speed = 800f;
     public float nextWaypointDistance = 1.2f;
-
     public Transform enemySprite;
 
     Transform target;
+
+    Transform _Enemytarget = null;
+    List<Transform> _AlternateEnemytarget;
+
     Path path;
     int currentWaypoint = 0;
     bool reachedEndofPath = false;
@@ -23,6 +26,9 @@ public class EnemyAI : MonoBehaviour
     Rigidbody2D rb;
     bool patroling = false;
     // Start is called before the first frame update
+
+
+
     void Start()
     {
         seeker = GetComponent<Seeker>();
@@ -31,6 +37,82 @@ public class EnemyAI : MonoBehaviour
         InvokeRepeating("UpdatePath", 0f, 0.5f);
         if(currentPatrolWaypoint < PatrolWaypoints.Length)
             target = PatrolWaypoints[currentPatrolWaypoint];
+        
+        _AlternateEnemytarget = new List<Transform>();
+    }
+
+    public Transform Enemytarget
+    {
+        get { return _Enemytarget; }
+        set {
+            if(_Enemytarget == value)
+            {
+                return;
+            }
+            //Debug.Log(value.name);
+            // if the proposed target is not the preferred one, we ignore it, provided none has been set
+            if(_Enemytarget != null && (((1 << value.gameObject.layer) & targetMask.value) == 0) ) 
+            {
+                Debug.Log("target: " + value.name + " ignored");
+                if(_AlternateEnemytarget.Contains(value) == false)//add the alternate target only once
+                {
+                    _AlternateEnemytarget.Add(value);//keep a list of all alternate targets in the area
+                }
+                return;
+            }
+            Debug.Log("target: " + value.name + " set");
+            
+            patroling = false;
+            reachedEndofPath = false;
+            currentWaypoint = 0;
+            if(_Enemytarget != null)//store the old target as an alternate
+            {
+                Debug.Log("old target: " + _Enemytarget.name + " now added to alternate targets");
+                if(_AlternateEnemytarget.Contains(_Enemytarget) == false)//add the alternate target only once
+                {
+                    _AlternateEnemytarget.Add(_Enemytarget);//keep a list of all alternate targets in the area
+                }  
+            }
+            _Enemytarget = value;
+            target = value;
+            seeker.StartPath(rb.position,target.position, OnPathComplete);//force path refresh
+        }
+    }
+
+    public void UnsetEnemyTarget(Transform oldTarget)
+    {
+        //remove old alternate target
+        if(_AlternateEnemytarget.Contains(oldTarget))
+        {
+            Debug.Log("removed " + oldTarget.name + " from alternate targets");
+            _AlternateEnemytarget.Remove(oldTarget);
+        }
+        //remove old primary target
+        if(_Enemytarget == oldTarget)
+        {   
+            if(_AlternateEnemytarget.Count > 0)
+            {
+                patroling = false;
+                reachedEndofPath = false;
+                currentWaypoint = 0;
+                _Enemytarget = _AlternateEnemytarget[0];
+                target = _AlternateEnemytarget[0];
+                seeker.StartPath(rb.position,target.position, OnPathComplete);//force path refresh
+
+                Debug.Log("removed " + oldTarget.name + ", now chasing "+ _Enemytarget.name);
+
+                _AlternateEnemytarget.RemoveAt(0);
+            }
+            else
+            {
+                Debug.Log("removed " + oldTarget.name + ", now patroling");
+                _Enemytarget = null;
+                target = PatrolWaypoints[currentPatrolWaypoint];
+                patroling = true;
+                seeker.StartPath(rb.position,target.position, OnPathComplete);//force path refresh
+            }
+        }
+
     }
 
     void UpdatePath()
@@ -105,35 +187,6 @@ public class EnemyAI : MonoBehaviour
                     currentPatrolWaypoint = 0;
                 }
                 target = PatrolWaypoints[currentPatrolWaypoint];
-            }
-            //to ensure we are not immediately retargetting
-            //if(currentWaypoint > 1)
-            {
-                //scan for squirrels vertical, and attack if seen
-                
-                RaycastHit2D hitInfo = Physics2D.CircleCast(transform.position, 2f ,Vector2.down, 1000f, targetMask);
-                if (hitInfo && hitInfo.transform.tag != "drone")
-                {
-                    //Debug.Log(hitInfo.transform.name);
-                    target = hitInfo.transform;
-                    patroling = false;
-                    reachedEndofPath = false;
-                    currentWaypoint = 0;
-                }
-                else // prioritize down over front
-                {
-                    //scan for drones horizontal, and attack if seen
-                    hitInfo = Physics2D.CircleCast(transform.position, 4f ,Vector2.left * enemySprite.localScale.x, 100f, targetMask);
-                    //Collider2D coll = Physics2D.OverlapBox(transform.position,new Vector2(100,10),0,targetMask);
-                    if(hitInfo && hitInfo.transform.tag == "drone")
-                    {
-                        //Debug.Log(coll.transform.name);
-                        target = hitInfo.transform;
-                        patroling = false;
-                        reachedEndofPath = false;
-                        currentWaypoint = 0;
-                    }
-                }
             }
         }
     }
---
+++ Assets/Scripts/TargetTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetTrigger : MonoBehaviour
+{
+    public GameObject receiver;
+    public LayerMask layer;
+    public string _tag;
+
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log(name + ":" + col.name);
+        if( ((1<<col.gameObject.layer) & layer.value) > 0)
+        {
+            //Debug.Log(name + ":" + col.name + " + layer");
+            if(_tag.Equals("") || _tag.Equals(col.transform.tag))
+            {
+                //Debug.Log(name + ":" + col.name + " + layer + tag");
+                var ai = receiver.GetComponent<EnemyAI>();
+                //Debug.Log("receiver:" + receiver.name);
+                if(ai != null)
+                    ai.Enemytarget = col.transform; 
+                else
+                    Debug.Log("no enemy AI");
+            }
+        }   
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        //Debug.Log(name + ":" + col.name);
+        if( ((1<<col.gameObject.layer) & layer.value) > 0)
+        {
+            //Debug.Log(name + ":" + col.name + " + layer");
+            if(_tag.Equals("") || _tag.Equals(col.transform.tag))
+            {
+                //Debug.Log(name + ":" + col.name + " + layer + tag");
+                var ai = receiver.GetComponent<EnemyAI>();
+                //Debug.Log("receiver:" + receiver.name);
+                if(ai != null)
+                    ai.UnsetEnemyTarget(col.transform);
+                else
+                    Debug.Log("no enemy AI");
+            }
+        }   
+    }
+}
