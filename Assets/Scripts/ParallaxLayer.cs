feecab68a33c14137966215815cac4aef5ce082d
git <robidev@github.com>
2020-10-04 23:48:59 +0200

    improved parallax scrolling, and extended puzzle

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -1,32 +1,34 @@
 using UnityEngine;
 
-namespace Platformer.View
+/// <summary>
+/// Used to move a transform relative to the main camera position with a scale factor applied.
+/// This is used to implement parallax scrolling effects on different branches of gameobjects.
+/// </summary>
+
+[ExecuteInEditMode]
+public class ParallaxLayer : MonoBehaviour 
 {
-    /// <summary>
-    /// Used to move a transform relative to the main camera position with a scale factor applied.
-    /// This is used to implement parallax scrolling effects on different branches of gameobjects.
-    /// </summary>
-    public class ParallaxLayer : MonoBehaviour
-    {
-        /// <summary>
-        /// Movement of the layer is scaled by this value.
-        /// </summary>
-        public Vector3 movementScale = Vector3.one;
+   public float referenceSize = 2;
 
-        Vector3 _offset;
+   public float xParallax = 0.2f;
+   public float yParallax = 0.2f;
+   public float sizeParallax = 0.2f;
+ 
+   void LateUpdate () 
+   {
+      float sizeRatio = (Camera.main.orthographicSize/referenceSize);
+      float newSize = (sizeRatio - 1) * sizeParallax + 1;
+  
+      Vector3 tempScale = transform.localScale;
+      tempScale.x = newSize;
+      tempScale.y = newSize;
+      transform.localScale = tempScale;
 
-        Transform _camera;
+      Vector3 tempPos = transform.position;
 
-        void Awake()
-        {
-            _offset = transform.position;
-            _camera = Camera.main.transform;
-        }
-
-        void LateUpdate()
-        {
-            transform.position = Vector3.Scale(_camera.position, movementScale) + _offset;
-        }
-
-    }
+      tempPos.x = Camera.main.transform.position.x * (1 -newSize + newSize*xParallax );
+      tempPos.y = Camera.main.transform.position.y * (1 -newSize + newSize*yParallax );
+        
+      transform.position = tempPos;
+   }
 }
