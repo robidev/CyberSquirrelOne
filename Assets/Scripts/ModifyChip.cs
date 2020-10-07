0de9ab0fc7c96163a970954374c0b54bbbc179bd
git <robidev@github.com>
2020-10-08 00:21:04 +0200

    improved puzzle1

---
+++ Assets/Scripts/ModifyChip.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//[ExecuteInEditMode]
+public class ModifyChip : MonoBehaviour
+{
+    public Puzzle1 puzzle1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int i = 1;
+        foreach(Toggle toggle in GetComponentsInChildren<Toggle>())
+        {
+            //toggle.name = i.ToString();
+            //i++;
+            //Add listener for when the state of the Toggle changes, to take action
+            toggle.onValueChanged.AddListener(delegate {
+                ToggleValueChanged(toggle);
+            });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ToggleValueChanged(Toggle toggle)
+    {
+        Transform obj = toggle.transform.Find("Background/Checkmark");
+        if(obj.GetComponent<Image>().color == Color.green && toggle.isOn == false)
+        {
+            obj.GetComponent<Image>().color = Color.red;
+            toggle.isOn = true;
+        }
+        else if(obj.GetComponent<Image>().color == Color.red && toggle.isOn == false)
+        {
+            obj.GetComponent<Image>().color = Color.green;
+        }   
+        Debug.Log("pin:" + toggle.name + " is " + toggle.isOn.ToString() );
+
+        //EEPROM and Microcontroller are connected, so essentially the same pin
+        int pin = 0;
+        int.TryParse(toggle.name, out pin);
+        if(pin == 45 || pin == 46)
+        {
+            setState(10,getState(45));
+            setState(11,getState(46));
+        }
+        if(pin == 16 || pin == 17)
+        {
+            setState(45,getState(10));
+            setState(46,getState(11));
+        }
+        puzzle1.chipModifier(pin);
+    }
+
+    public int getState(int pin)
+    {
+        foreach(Transform child in transform)
+        {
+            Transform res = child.Find(pin.ToString());
+            if(res != null)
+            {
+                Toggle toggle = res.GetComponent<Toggle>();
+                if(toggle.isOn == true)
+                {
+                    Transform obj = toggle.transform.Find("Background/Checkmark");
+                    if(obj.GetComponent<Image>().color == Color.green)
+                    {
+                        return 1;
+                    }
+                    else //obj.GetComponent<Image>().color == Color.red
+                    {
+                        return 2;
+                    }   
+                }
+                else //toggle.isOn == false
+                {
+                    return 0;
+                }
+            }
+        }
+        return -1; // pin not found
+    }
+
+    public void setState(int pin, int state)
+    {
+        foreach(Transform child in transform)
+        {
+            Transform res = child.Find(pin.ToString());
+            if(res != null)
+            {
+                Toggle toggle = res.GetComponent<Toggle>();
+                if(state > 0)
+                {
+                    Transform obj = toggle.transform.Find("Background/Checkmark");
+                    if(state == 1)
+                    {
+                        obj.GetComponent<Image>().color = Color.green;
+                    }
+                    else //obj.GetComponent<Image>().color == Color.red
+                    {
+                        obj.GetComponent<Image>().color = Color.red;
+                    }   
+                    toggle.isOn = true;
+                }
+                else //state is off
+                {
+                    toggle.isOn = false;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Puzzle1.cs
+++ Assets/Scripts/Puzzle1.cs
@@ -6,13 +6,12 @@ using UnityEngine.UI;
 
 public class Puzzle1 : MonoBehaviour
 {
+    //TODO: buzzer?
     public Text LCD;
     private string _input = "";
-
     private string welcomeText = "Enter code:";
     private bool PuzzleSolved = false;
     private bool KeypadEnabled = true;
-    private bool EEPROM_WP_enable = false;
     private int[] buffer;
     private int PASSWORD_ADDRESS = 1;
     private int PASSWORD_LENGTH_ADDRESS = 0;
@@ -21,16 +20,17 @@ public class Puzzle1 : MonoBehaviour
     public int MaxTries = 10;
     public Image keypad;
 
+    public Sprite both;
     public Sprite granted;
     public Sprite denied;
     public Sprite normal;
     public UnityEvent UnlockEvent;
-
+    public ModifyChip modifyChip;
     // Start is called before the first frame update
     void Start()
     {
-        LCD.text = welcomeText;
-        keypad.sprite = normal;
+        LCD_write(welcomeText,true);
+        LED_set(false,false);
         buffer = new int[256];
         EEPROM_write(PASSWORD_LENGTH_ADDRESS, 4);
         EEPROM_write(TRIES_ADDRESS, 0);
@@ -41,8 +41,8 @@ public class Puzzle1 : MonoBehaviour
     {
         _input = "";
         KeypadEnabled = true; //enable keypad
-        keypad.sprite = normal;
-        LCD.text = welcomeText;
+        LED_set(false,false);
+        LCD_write(welcomeText,true);
         buffer = new int[256];
         EEPROM_write(PASSWORD_LENGTH_ADDRESS, 4);
         EEPROM_write(TRIES_ADDRESS, 0);
@@ -53,6 +53,28 @@ public class Puzzle1 : MonoBehaviour
     private void Update()
     {
         Time.timeScale = 0;
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                ButtonInput("x");
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                ButtonInput("v");
+            }
+            else if(c == 'q')
+            {
+                ButtonInput("q");
+            }
+            else
+            {
+                if(c >= '0' && c <= '9')
+                {
+                    ButtonInput(c.ToString());
+                }
+            }
+        }
     }
 
     public void SetPassword()
@@ -69,12 +91,15 @@ public class Puzzle1 : MonoBehaviour
 
     public void ButtonInput(string input)
     {
+        //
+        //scancode override logic
+        //
         if(input == "q")
         {
             _input = "";
             KeypadEnabled = true; //enable keypad
-            keypad.sprite = normal;
-            LCD.text = welcomeText;
+            LED_set(false,false);
+            LCD_write(welcomeText,true);
             gameObject.SetActive(false);
             Time.timeScale = 1;
             return;
@@ -85,23 +110,23 @@ public class Puzzle1 : MonoBehaviour
             if(input == "x") //backspace
             {
                 //Debug.Log("rem: " + welcomeText.Length);
-                if(LCD.text.Length > welcomeText.Length)
+                if(LCD_length() > welcomeText.Length)
                 {
-                    LCD.text = LCD.text.Remove(LCD.text.Length-1);
+                    LCD_backspace();
                     _input = _input.Remove(_input.Length-1);
                 }
             }
             else //if input is ok or a number
             {
                 int passwordLength = EEPROM_read(PASSWORD_LENGTH_ADDRESS);
-                if(input != "v" && LCD.text.Length < welcomeText.Length + passwordLength) // add number if its below password length
+                if(input != "v" && LCD_length() < welcomeText.Length + passwordLength) // add number if its below password length
                 {
-                    LCD.text += "*";
+                    LCD_write("*",false);
                     _input += input;
                 }
                 if(input == "v" ) //pressed ok, so check password
                 {
-                    if(LCD.text.Length < welcomeText.Length + passwordLength) //pad the password if it is not matching length
+                    if(LCD_length() < welcomeText.Length + passwordLength) //pad the password if it is not matching length
                     {
                         _input = _input.PadRight(passwordLength,'A');//pad it with character A, as it will never match a code
                     }
@@ -115,18 +140,31 @@ public class Puzzle1 : MonoBehaviour
     {
         KeypadEnabled = false;//disable the keypad input
         Debug.Log("checking:" + _input);
-        LCD.text = "Validating";
+        LCD_write("Validating", true);
         yield return new WaitForSecondsRealtime(1f);
 
         int passwordLength = EEPROM_read(PASSWORD_LENGTH_ADDRESS);
+        if(passwordLength == 0)
+        {
+            LCD_write("No password set!",true);
+            LED_set(false,true); //enable red led
+            yield return new WaitForSecondsRealtime(1f);
+
+            //reset the lock again for input
+            _input = "";
+            LCD_write(welcomeText,true); //default text
+            LED_set(false,false); //disable leds
+            KeypadEnabled = true; //enable keypad  
+            yield break;          
+        }
         for(int i = 0; i < passwordLength; i++)//check each character
         {
             if(_input[i] != EEPROM_read(PASSWORD_ADDRESS + i).ToString()[0]) //if character does not match password, stop with checking and show error
             {
                 //yield return new WaitForSecondsRealtime(1f);
 
-                LCD.text = "Incorrect password";
-                keypad.sprite = denied; //enable red led
+                LCD_write("Incorrect password",true);
+                LED_set(false,true); //enable red led
                 yield return new WaitForSecondsRealtime(1f);
 
                 int tries = EEPROM_read(TRIES_ADDRESS);//retrieve tries from EEPROM
@@ -134,28 +172,29 @@ public class Puzzle1 : MonoBehaviour
                 {
 
                     EEPROM_write(TRIES_ADDRESS, tries + 1);//increment tries in EEPROM
-                    LCD.text = (MaxTries - tries) + " tries left";//display the amount of tries left
+                    LCD_write((MaxTries - tries) + " tries left",true);//display the amount of tries left
                     yield return new WaitForSecondsRealtime(1f);
 
                     //reset the lock again for input
                     _input = "";
-                    LCD.text = welcomeText; //default text
-                    keypad.sprite = normal; //disable leds
+                    LCD_write(welcomeText,true); //default text
+                    LED_set(false,false); //disable leds
                     KeypadEnabled = true; //enable keypad
                 }
                 else
                 {
                     //lock out the puzzle
-                    LCD.text = "Too many failed attemts";
-                    keypad.sprite = denied; //enable red led
+                    LCD_write("Too many failed attemts",true);
+                    LED_set(false,true);//enable red led
+                    
                     //KeypadEnabled = false; //keep keypad disabled
                     SetPassword(); //reset password
                     yield return new WaitForSecondsRealtime(1f); 
 
                     _input = "";
                     KeypadEnabled = true; //enable keypad
-                    keypad.sprite = normal;
-                    LCD.text = welcomeText;
+                    LED_set(false,false);
+                    LCD_write(welcomeText,true);
                     EEPROM_write(TRIES_ADDRESS, 0);
                     //kick out of lock-puzzle
                     gameObject.SetActive(false);
@@ -163,18 +202,18 @@ public class Puzzle1 : MonoBehaviour
                 }
                 yield break;
             }
-            LCD.text += ".";
+            LCD_write(".",false);
             yield return new WaitForSecondsRealtime(1f); 
         }
 
         Debug.Log("code OK");
-        LCD.text = "Access granted";
-        keypad.sprite = granted; //enable green led
+        LCD_write("Access granted",true);
+        LED_set(true,false);//enable green led
         PuzzleSolved = true;
         UnlockEvent.Invoke();
         yield return new WaitForSecondsRealtime(1f);
 
-        keypad.sprite = normal; //disable leds
+        LED_set(false,false); //disable leds
         EEPROM_write(TRIES_ADDRESS, 0); //reset tries
         KeypadEnabled = false; //keep keypad disabled
         yield return new WaitForSecondsRealtime(1f);
@@ -184,19 +223,151 @@ public class Puzzle1 : MonoBehaviour
 
     private int EEPROM_read(int address)
     {
+        //TODO: add address pins
+        int SDA = modifyChip.getState(45);
+        int SCL = modifyChip.getState(46);
+        if(SCL != 0 || SDA == 2)
+            return 0;
+        if(SDA == 1)
+            return 0xff;
+
         return buffer[address % 256];
     }
-
     private void EEPROM_write(int address, int value)
     {
-        if(EEPROM_WP_enable == false) //if EEPROM was not modified
+        //TODO: add address pins
+        int SDA = modifyChip.getState(45);
+        int SCL = modifyChip.getState(46);
+        if(SCL != 0 || SDA != 0)
+            return;
+
+        int EEPROM_WP_enable = modifyChip.getState(47);
+        if(EEPROM_WP_enable != 1) //if EEPROM was not forced high
         {
             buffer[address % 256] = value;
         }
     }
+    //https://www.electronicsforu.com/resources/learn-electronics/16x2-lcd-pinout-diagram
+    private void LCD_write(string value, bool clear)
+    {
+        
+        int RS = modifyChip.getState(15);// pin 15 = RS
+        
+        int RW = modifyChip.getState(16);// pin 16 = RW,
+        if(RS == 2 || RW == 1) // if command register forced(low), or RW forced read(high); just return
+            return;
+        
+        //1-8 = D0-D7
+        string newVal = "";
+        foreach(char letter in value)
+        {
+            int modifiedLetter = letter;
+            int[] a = new int[8];
+            for(int i = 0; i < 9; i++)
+            {
+                int bit = modifyChip.getState(i+1);
+                if(bit == 1)
+                {
+                    modifiedLetter |= (1 << i);
+                }
+                else if(bit == 2)
+                {
+                    modifiedLetter &= ~(1 << i);
+                }
+            }
+            newVal += (char)modifiedLetter;
+        }
+        
+        if(clear && RS == 0) // we cannot clear LCD if command register is forced
+            LCD.text = newVal;
+        else
+            LCD.text += newVal;
+    }
+    private void LCD_backspace()
+    {
+        int RS = modifyChip.getState(15);// pin 15 = RS
+        int RW = modifyChip.getState(16);// pin 16 = RW,
+        if(RS == 1 || RW == 1) // if data register forced(high), or RW forced read(high); just return
+            return;
+        LCD.text = LCD.text.Remove(LCD.text.Length-1);
+    }
+    private int LCD_length()
+    {
+        return LCD.text.Length;
+    }
+    private void LED_set(bool green, bool red)
+    {
+        int state = modifyChip.getState(38); // green led
+        if(state > 0){
+            if(state == 1) green = true;
+            else green = false;
+        }
+        state = modifyChip.getState(39); // red led
+        if(state > 0){
+            if(state == 1) red = true;
+            else red = false;
+        }
 
+        if(green == true && red == true)
+        {
+            keypad.sprite = both; 
+        }
+        else if(green == true)
+        {
+            keypad.sprite = granted; 
+        }
+        else if(red == true)
+        {
+            keypad.sprite = denied; 
+        }
+        else
+        {
+            keypad.sprite = normal;
+        }
+    }
+
+    private void LED_get(out bool green, out bool red)
+    {
+        if(keypad.sprite == both)
+        {
+            green = true;
+            red = true;
+        }
+        else if(keypad.sprite == granted)
+        {
+            green = true;
+            red = false;
+        }
+        else if(keypad.sprite == denied)
+        {
+            green = false;
+            red = true;
+        }
+        else
+        {
+            green = false;
+            red = false;
+        }
+
+    }
     public bool IsPuzzleSolved()
     {
         return PuzzleSolved;
+    }
+
+    public void chipModifier(int pin)
+    {
+        switch(pin)
+        {
+            case 38:
+            case 39:
+                bool red;
+                bool green;
+                LED_get(out green, out red);
+                LED_set(green,red);
+            break;
+            default:
+            break;
+        }
     }
 }
