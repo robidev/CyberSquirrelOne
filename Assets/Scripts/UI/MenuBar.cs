2d84b5e82d88ce37ee51dc8e73a5c68acbbb2899
git <robidev@github.com>
2020-12-22 00:53:36 +0100

    improved menubar

---
+++ Assets/Scripts/UI/MenuBar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class MenuBar : MonoBehaviour
+{
+    public Transform MenuBarPanel;
+    public GameObject MenuBarItem;
+    public GameObject MenuBarItemMenu;
+    public GameObject MenuBarItemButton;
+
+    public string[] menubarItems;
+    List<GameObject> menuBarItems = new List<GameObject>();
+    void Start()
+    {
+        foreach(string item in menubarItems)
+        {
+            if(item[0] == '+')
+            {
+                GameObject menuBarItem = Instantiate(MenuBarItem, MenuBarPanel) as GameObject;
+                Transform name = menuBarItem.transform.Find("Name");
+                if(name != null)
+                {
+                    name.GetComponent<TextMeshProUGUI>().text = item.Substring(1);
+                }
+                menuBarItems.Add(menuBarItem);
+                LayoutRebuilder.ForceRebuildLayoutImmediate(MenuBarPanel.GetComponent<RectTransform>());
+            }
+        }
+        //this is needed for the reposition of the buttons
+        LayoutRebuilder.ForceRebuildLayoutImmediate(MenuBarPanel.GetComponent<RectTransform>());
+    
+        GameObject menuBarItemMenu = null;
+        int i = 0;
+        foreach(string item in menubarItems)
+        {
+            if(item[0] == '+')
+            {
+                menuBarItemMenu = Instantiate(MenuBarItemMenu, menuBarItems[i].GetComponent<RectTransform>()) as GameObject;//get location from button
+                menuBarItems[i].GetComponent<MenuBarButtonHelper>().SubMenu = menuBarItemMenu;
+                menuBarItemMenu.transform.SetParent(MenuBarPanel.parent.GetComponent<RectTransform>(),true);//change parent to prevent layout rebuild
+                menuBarItemMenu.SetActive(false);
+                i++;
+            }
+            else if(menuBarItemMenu != null)
+            {
+                GameObject menuBarItemButton = Instantiate(MenuBarItemButton, menuBarItemMenu.transform) as GameObject;
+                menuBarItemButton.GetComponent<Button>().onClick.AddListener(() => {Debug.Log("clicked:" + item);});
+                menuBarItemButton.GetComponentInChildren<TextMeshProUGUI>().text = item;
+            }
+        }      
+    }
+}
---
+++ Assets/Scripts/UI/MenuBarButtonHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class MenuBarButtonHelper : MonoBehaviour, ISelectHandler, IDeselectHandler// required interface when using the OnSelect method.
+{
+    public GameObject SubMenu;
+	//Do this when the selectable UI object is selected.
+	public void OnSelect (BaseEventData eventData) 
+	{
+		//Debug.Log (this.gameObject.name + " was selected");
+        SubMenu.SetActive(true);
+	}
+
+	public void OnDeselect (BaseEventData eventData) 
+	{
+		//Debug.Log (this.gameObject.name + " was deselected");
+		StartCoroutine(DelayDisable());
+	}    
+
+	IEnumerator DelayDisable()
+	{
+		yield return new WaitForSeconds(0.1f);
+		SubMenu.SetActive(false);
+	}
+}
