b268b68d2ae62807b4290ac3a4fbf6ccc40d8f67
git <robidev@github.com>
2020-12-22 15:54:47 +0100

    commit menu-bar

--- Assets/Scripts/UI/MenuBar.cs
+++ Assets/Scripts/UI/MenuBar.cs
@@ -3,53 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class MenuBar : MonoBehaviour
 {
+    [System.Serializable]
+    public class SubMenuBarItemClass
+    {
+        public string name;
+        [HideInInspector] public GameObject element;
+        public UnityEvent onClick;
+    }
+
+    [System.Serializable]
+    public class MenuBarItemClass
+    {
+        public string name;
+        [HideInInspector] public GameObject element;
+        [HideInInspector] public GameObject subElement;
+        public List<SubMenuBarItemClass> subMenuItems;
+    }
     public Transform MenuBarPanel;
     public GameObject MenuBarItem;
     public GameObject MenuBarItemMenu;
     public GameObject MenuBarItemButton;
-
-    public string[] menubarItems;
-    List<GameObject> menuBarItems = new List<GameObject>();
+    public GameObject MenuBarItemDivider;
+    public List<MenuBarItemClass> menuBarItems;
+    //public string[] menubarItems;
+    //List<GameObject> menuBarItems = new List<GameObject>();
     void Start()
     {
-        foreach(string item in menubarItems)
+        foreach(MenuBarItemClass MenuItem in menuBarItems)
         {
-            if(item[0] == '+')
+            MenuItem.element = Instantiate(MenuBarItem, MenuBarPanel) as GameObject;
+            Transform name = MenuItem.element.transform.Find("Name");
+            if(name != null)
             {
-                GameObject menuBarItem = Instantiate(MenuBarItem, MenuBarPanel) as GameObject;
-                Transform name = menuBarItem.transform.Find("Name");
-                if(name != null)
-                {
-                    name.GetComponent<TextMeshProUGUI>().text = item.Substring(1);
-                }
-                menuBarItems.Add(menuBarItem);
-                LayoutRebuilder.ForceRebuildLayoutImmediate(MenuBarPanel.GetComponent<RectTransform>());
+                name.GetComponent<TextMeshProUGUI>().text = MenuItem.name;
             }
+            LayoutRebuilder.ForceRebuildLayoutImmediate(MenuBarPanel.GetComponent<RectTransform>());
         }
         //this is needed for the reposition of the buttons
         LayoutRebuilder.ForceRebuildLayoutImmediate(MenuBarPanel.GetComponent<RectTransform>());
     
-        GameObject menuBarItemMenu = null;
-        int i = 0;
-        foreach(string item in menubarItems)
+        foreach(MenuBarItemClass MenuItem in menuBarItems)
         {
-            if(item[0] == '+')
+            //create a submenu panel under the button and hide it
+            MenuItem.subElement = Instantiate(MenuBarItemMenu, MenuItem.element.GetComponent<RectTransform>()) as GameObject;//get location from button
+            MenuItem.element.GetComponent<MenuBarButtonHelper>().SubMenu = MenuItem.subElement;
+            MenuItem.subElement.transform.SetParent(MenuBarPanel.parent.GetComponent<RectTransform>(),true);//change parent to prevent layout rebuild
+            MenuItem.subElement.SetActive(false);
+
+            //create the submenu buttons
+            foreach(SubMenuBarItemClass SubMenuItem in MenuItem.subMenuItems)
             {
-                menuBarItemMenu = Instantiate(MenuBarItemMenu, menuBarItems[i].GetComponent<RectTransform>()) as GameObject;//get location from button
-                menuBarItems[i].GetComponent<MenuBarButtonHelper>().SubMenu = menuBarItemMenu;
-                menuBarItemMenu.transform.SetParent(MenuBarPanel.parent.GetComponent<RectTransform>(),true);//change parent to prevent layout rebuild
-                menuBarItemMenu.SetActive(false);
-                i++;
-            }
-            else if(menuBarItemMenu != null)
-            {
-                GameObject menuBarItemButton = Instantiate(MenuBarItemButton, menuBarItemMenu.transform) as GameObject;
-                menuBarItemButton.GetComponent<Button>().onClick.AddListener(() => {Debug.Log("clicked:" + item);});
-                menuBarItemButton.GetComponentInChildren<TextMeshProUGUI>().text = item;
+                if(SubMenuItem.name == "---")
+                {
+                    SubMenuItem.element = Instantiate(MenuBarItemDivider, MenuItem.subElement.transform) as GameObject;
+                }
+                else
+                {
+                    SubMenuItem.element = Instantiate(MenuBarItemButton, MenuItem.subElement.transform) as GameObject;
+                    SubMenuItem.element.GetComponent<Button>().onClick.AddListener(() => {Debug.Log("clicked:" + SubMenuItem.name);});
+                    SubMenuItem.element.GetComponentInChildren<TextMeshProUGUI>().text = SubMenuItem.name;
+                }
+
             }
         }      
     }
