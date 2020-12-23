6a4797f3a6070966ba3c95752ab88c4a1146119f
git <robidev@github.com>
2020-12-23 23:51:11 +0100

    improved video capability in webgl

--- Assets/Scripts/UI/VideoStream.cs
+++ Assets/Scripts/UI/VideoStream.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Video;
 
 public class VideoStream : MonoBehaviour
 {
@@ -13,11 +14,26 @@ public class VideoStream : MonoBehaviour
     public UnityEvent EndTrigger;
 
 
-    public UnityEngine.Video.VideoPlayer videoPlayer;
+    public VideoPlayer videoPlayer;
+    public VideoClip playOnStart;
+    public string playOnStartUrl;
+    public VideoClip playOnEvent;
+    public string playOnEventUrl;
+    public VideoClip playOnEnd;
+    public string playOnEndUrl;
     void Start()
     {
         videoPlayer.loopPointReached += EndReached;
-        videoPlayer.started += StartReached;
+        videoPlayer.started += StartReached;        
+        #if UNITY_WEBGL && !UNITY_EDITOR
+            Debug.Log("WEBGL movie handling (URL: '" + Application.streamingAssetsPath + "/" + playOnStartUrl + "')");
+            videoPlayer.source = VideoSource.Url;
+            videoPlayer.url = Application.streamingAssetsPath + "/" + playOnStartUrl;
+        #else
+            Debug.Log("normal movie handling (Clip)");
+            videoPlayer.source = VideoSource.VideoClip;
+            videoPlayer.clip = playOnStart;
+        #endif
     }
     void StartReached(UnityEngine.Video.VideoPlayer vp)
     {
@@ -41,6 +57,13 @@ public class VideoStream : MonoBehaviour
         {
              Debug.Log("endreached");
             EndTrigger.Invoke();
+            videoPlayer.clip = playOnEnd;
+            #if UNITY_WEBGL && !UNITY_EDITOR
+                videoPlayer.url = Application.streamingAssetsPath + "/" + playOnEndUrl;
+            #else
+                videoPlayer.clip = playOnEnd;
+            #endif
+            
         }
     }
 
@@ -52,5 +75,17 @@ public class VideoStream : MonoBehaviour
     public void SetStartedCheck(bool value)
     {
         StartedCheck = value;
+    }
+
+    public void TriggerEventVideo()
+    {
+        StartedCheck = true;
+        EndReachedCheck = true;
+        #if UNITY_WEBGL && !UNITY_EDITOR
+            videoPlayer.url = Application.streamingAssetsPath + "/" + playOnEventUrl;
+        #else
+            videoPlayer.clip = playOnEvent;
+        #endif
+        videoPlayer.isLooping = false;
     }
 }
