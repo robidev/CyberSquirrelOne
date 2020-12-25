9b723d9d2606725cfcb3e9af974a62f4f31b536d
git <robidev@github.com>
2020-12-26 00:32:29 +0100

    added windows

--- Assets/Scripts/UI/EditorWindow.cs
+++ Assets/Scripts/UI/EditorWindow.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EditorWindow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField TextField;
+
+    public void DisplayText(string text)
     {
-        
+        TextField.text = text;
+        gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplayTextResourceByName(string resourceName)
     {
-        
+        TextAsset text = Resources.Load(resourceName) as TextAsset;
+        TextField.text = text.text;
+        gameObject.SetActive(true);
+    }
+
+        public void DisplayTextResourceObject(TextAsset TextResource)
+    {
+        TextField.text = TextResource.text;
+        gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/UI/ImageWindow.cs
+++ Assets/Scripts/UI/ImageWindow.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageWindow : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public Image image;
+
+    public void DisplayImage(Image _image)
     {
-        
+        image = _image;
+        gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplaySprite(Sprite _sprite)
     {
-        
+        image.sprite = _sprite;
+        gameObject.SetActive(true);
     }
 }
