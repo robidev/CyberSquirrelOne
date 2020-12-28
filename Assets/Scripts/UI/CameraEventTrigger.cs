7fff34ee5646aefe515b9b49a2809a910c795d3d
git <robidev@github.com>
2020-12-28 23:05:20 +0100

    replaced video script

---
+++ Assets/Scripts/UI/CameraEventTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEventTrigger : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+        GameObject text_online = animator.transform.parent.Find("dashboard").Find("cam2_online").gameObject;
+        GameObject text_offline = animator.transform.parent.Find("dashboard").Find("cam2_offline").gameObject;
+        text_online.SetActive(false);
+        text_offline.SetActive(true);
+        animator.transform.parent.GetComponent<ScreenOverlay>().DestroyOverlay();
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Scripts/UI/ScreenOverlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class ScreenOverlay : MonoBehaviour
+{
+    public GameObject ModalOverLayPrefab;
+    GameObject overlay;
+    public void DoOverlay()
+    {
+        overlay = Instantiate(ModalOverLayPrefab,transform.parent);
+        overlay.GetComponent<RectTransform>().anchorMin = Vector2.zero;
+        overlay.GetComponent<RectTransform>().anchorMax = Vector2.one;
+        overlay.GetComponent<RectTransform>().sizeDelta = Vector2.zero;
+        overlay.GetComponent<RectTransform>().SetAsLastSibling();//set top background
+    }//Destroy(overlay);
+
+    public void DestroyOverlay()
+    {
+        Destroy(overlay);
+    }
+}
