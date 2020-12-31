d8ac5df6419a07c7a7a405cd4b4273039fcf0caf
git <robidev@github.com>
2020-12-31 14:41:57 +0100

    added calendar

--- Assets/Scripts/UI/CalendarWindow.cs
+++ Assets/Scripts/UI/CalendarWindow.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CalendarWindow : MonoBehaviour
 {
+    public Transform Calendar;
+    public TMP_Text MonthYear;
+    public GameObject DayPrefab;
+    private GameObject[] Days = new GameObject[40];
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
+        MonthYear.text = System.DateTime.Now.ToString("MMMM, yyyy");
+        int year = System.DateTime.Now.Year;
+        int month = System.DateTime.Now.Month;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        System.DateTime cc = new System.DateTime(year,month,1);
+        int weekday = (int)cc.DayOfWeek;
+        int day = 1;
+        for(int i = 0; i < 40; i++)
+        {
+            Days[i] = Instantiate(DayPrefab,Calendar);
+            if(i < weekday)
+            {
+                Days[i].transform.Find("Number").gameObject.SetActive(false);
+            }
+            else
+            {
+                try
+                {
+                    System.DateTime dd = new System.DateTime(year,month,day);
+                    Days[i].transform.Find("Number").GetComponentInChildren<TMP_Text>().text = dd.Day.ToString();
+                    if(dd.DayOfYear == System.DateTime.Now.DayOfYear)
+                    {
+                        Days[i].GetComponent<Image>().color = Color.white;
+                    }
+                }
+                catch 
+                {
+                    Days[i].SetActive(false);
+                }
+
+                day++;
+            }
+        }
     }
 }
